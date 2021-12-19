df763c43c26f708257faaf7e9016e41c1e130cc1
Joseph Madras <josephkabemba77@gmail.com>
2021-12-19 23:17:54 +0100

    feat: create a Button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,11 @@
+const Button = (props) => {
+  const { variant = "primary", children, ...rest } = props;
+
+  return (
+    <button className={`button ${variant}`} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
